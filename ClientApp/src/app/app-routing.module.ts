ad3448d62d14e3baa6d41f988438e6a699079c45
kar7777 <karla_vanesa_21@hotmail.com>
2020-07-17 22:54:54 -0600

    change view start game

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { Routes, RouterModule }      from '@angular/router';
+import { StartGameComponent } from './startgame/startgame.component';
 import { MainViewComponent } from './main-view/main-view.component';
 
 const appRoutes: Routes = [
-    { path: 'home', component: MainViewComponent },
-    { path: '', redirectTo: 'home', pathMatch: 'full' }
+    { path: 'start', component: StartGameComponent },
+    { path: '', redirectTo: 'start', pathMatch: 'full' },
+    {path: 'main-view', component: MainViewComponent},
+    {path: '', redirectTo: 'main-view', pathMatch: 'full'}
 ];
 
 export const APP_ROUTES = RouterModule.forRoot( appRoutes );
---
+++ ClientApp/src/app/startgame/startgame.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-startgame',
+    templateUrl: './startgame.component.html',
+    styleUrls: ['./startgame.component.css']
+  })
+  export class StartGameComponent{
+  }  
+    
